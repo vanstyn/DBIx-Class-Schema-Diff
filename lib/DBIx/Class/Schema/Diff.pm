31de834b41666487947f21bb4e5212a8441c7e2b
Henry Van Styn <vanstyn@intellitree.com>
2014-04-22 16:10:03 -0400

    POD tweaks

--- lib/DBIx/Class/Schema/Diff.pm
+++ lib/DBIx/Class/Schema/Diff.pm
@@ -282,12 +282,13 @@ as C<old_schema>.
 
 =head2 diff
 
-Returns the differences between the the two schemas as a HashRef structure, or C<undef> if there are none.
+Returns the differences between the two schemas as a HashRef structure, or C<undef> if there are 
+none.
 
-The HashRef is divided first by source name, then by type, with the special C<_event> key identifying
-the type of change (added, deleted or changed) at both the source and the type level. For 'changed' events
-within types, a deeper, type-specific diff HashRef is provided (with column_info/relationship_info diffs 
-generated using L<Hash::Diff>).
+The HashRef is divided first by source name, then by type, with the special C<_event> key 
+identifying the kind of modification (added, deleted or changed) at both the source and the type 
+level. For 'changed' events within types, a deeper, type-specific diff HashRef is provided (with 
+column_info/relationship_info diffs generated using L<Hash::Diff>).
 
 Here is an example of what a diff packet (with a sampling of lots of different kinds of changes) 
 might look like:
@@ -360,54 +361,59 @@ See L<FILTERING|DBIx::Class::Schema::Diff#FILTERING> for filter argument syntax.
 
 =head1 FILTERING
 
-The L<filter|DBIx::Class::Schema::Diff#filter> (and inverse L<filter_out|DBIx::Class::Schema::Diff#filter_out>) 
-method is analogous to ResultSet's L<search_rs|DBIx::Class::ResultSet#search_rs> in that it is chainable 
-(i.e. returns a new object instance) and each call further restricts the data considered. But, instead of 
-building up an SQL query, it filters the data in the HashRef returned by L<diff|DBIx::Class::Schema::Diff#diff>. 
+The L<filter|DBIx::Class::Schema::Diff#filter> (and inverse 
+L<filter_out|DBIx::Class::Schema::Diff#filter_out>) method is analogous to ResultSet's 
+L<search_rs|DBIx::Class::ResultSet#search_rs> in that it is chainable (i.e. returns a new object 
+instance) and each call further restricts the data considered. But, instead of building up an SQL 
+query, it filters the data in the HashRef returned by L<diff|DBIx::Class::Schema::Diff#diff>. 
 
-The filter argument(s) define an expression which matches specific parts of the C<diff> packet. In the case of 
-C<filter()>, all data that B<does not> match the expression is removed from the diff HashRef (of the returned,
-new object), while in the case of C<filter_out()>, all data that B<does> match the expression is removed.
+The filter argument(s) define an expression which matches specific parts of the C<diff> packet. In 
+the case of C<filter()>, all data that B<does not> match the expression is removed from the diff 
+HashRef (of the returned, new object), while in the case of C<filter_out()>, all data that B<does> 
+match the expression is removed.
 
-The filter expression is designed to be simple and declarative. It can be supplied as a list of strings
-which match schema data either broadly or narrowly. A filter string argument follows this general pattern:
+The filter expression is designed to be simple and declarative. It can be supplied as a list of 
+strings which match schema data either broadly or narrowly. A filter string argument follows this 
+general pattern:
 
  '<source>:<type>/<id>'
 
-Where C<source> is the name of a specific source in the schema (either side), C<type> is the I<type> of data, which is currently 
-one of five (5) supported, predefined types: I<'columns'>, I<'relationships'>, I<'constraints'>, I<'isa'> and I<'table_name'>,
-and C<id> is the name of an item, specific to that type, if applicable. 
+Where C<source> is the name of a specific source in the schema (either side), C<type> is the 
+I<type> of data, which is currently one of five (5) supported, predefined types: I<'columns'>, 
+I<'relationships'>, I<'constraints'>, I<'isa'> and I<'table_name'>, and C<id> is the name of an 
+item, specific to that type, if applicable. 
 
-For instance, this expression would match only the I<column> named 'timestamp' in the source named 'Artist':
+For instance, this expression would match only the I<column> named 'timestamp' in the source 
+named 'Artist':
 
- 'Artist:column/timestamp'
+ 'Artist:columns/timestamp'
 
-Not all types have sub-items (only I<columns>, I<relationships> and I<constraints>). The I<isa> and I<table_name>
-types are source-global. So, for example, to see changes to I<isa> (i.e. differences in inheritance and/or
-loaded components in the result class) you could use the following:
+Not all types have sub-items (only I<columns>, I<relationships> and I<constraints>). The I<isa> and 
+I<table_name> types are source-global. So, for example, to see changes to I<isa> (i.e. differences 
+in inheritance and/or loaded components in the result class) you could use the following:
 
  'Artist:isa'
 
-On the other hand, not only are there multiple I<columns> and I<relationships> within each source, but
-each can have specific changes to their attributes (column_info/relationship_info) which can also be 
-targeted selectively. For instance, to match only changes in C<size> of a specific column:
+On the other hand, not only are there multiple I<columns> and I<relationships> within each source, 
+but each can have specific changes to their attributes (column_info/relationship_info) which can 
+also be targeted selectively. For instance, to match only changes in C<size> of a specific column:
 
- 'Artist:column/timestamp.size'
+ 'Artist:columns/timestamp.size'
 
-Attributes with sub hashes can be matched as well. For example, to match only changes in C<list> I<within>
-C<extra> (which is where DBIC puts the list of possible values for enum columns):
+Attributes with sub hashes can be matched as well. For example, to match only changes in C<list> 
+I<within> C<extra> (which is where DBIC puts the list of possible values for enum columns):
 
- 'Artist:column/my_enum.extra.list'
+ 'Artist:columns/my_enum.extra.list'
 
 The structure is specific to the type. The dot-separated path applies to the data returned by L<column_info|DBIx::Class::ResultSource#column_info> for columns and
 L<relationship_info|DBIx::Class::ResultSource#relationship_info> for relationships. For instance, 
-the following matches changes to C<cascade_delete> of a specific relationship named 'some_rel' in the 'Artist'
-source:
+the following matches changes to C<cascade_delete> of a specific relationship named 'some_rel' 
+in the 'Artist' source:
 
- 'Artist:relationship/some_rel.attrs.cascade_delete'
+ 'Artist:relationships/some_rel.attrs.cascade_delete'
 
-Filter arguments can also match I<broadly> using the wildcard asterisk character (C<*>). For instance, to match
-I<'isa'> changes in any source:
+Filter arguments can also match I<broadly> using the wildcard asterisk character (C<*>). For 
+instance, to match I<'isa'> changes in any source:
 
  '*:isa'
 
@@ -416,26 +422,26 @@ be written simply as:
 
  'isa'
 
-This is possible because 'isa' is understood/known as a I<type> keyword. Additionally, the system knows the names
-of all the sources in advance, so the following filter string argument would match everything in the 'Artist'
-source:
+This is possible because 'isa' is understood/known as a I<type> keyword. Additionally, the system 
+knows the names of all the sources in advance, so the following filter string argument would match 
+everything in the 'Artist' source:
 
  'Artist'
 
-Sub-item names are automatically resolved, too. The following would match any column, relationship, or
-constraint named C<'code'> in any source:
+Sub-item names are automatically resolved, too. The following would match any column, relationship, 
+or constraint named C<'code'> in any source:
 
  'code'
 
-When you have schemas with overlapping names, such as a column named 'isa', you simply need to supply more
-specific match strings, as ambiguous names are resolved with left-precedence. So, to match any
-column, relationship, or constraint named 'isa', you could use the following:
+When you have schemas with overlapping names, such as a column named 'isa', you simply need to 
+supply more specific match strings, as ambiguous names are resolved with left-precedence. So, to 
+match any column, relationship, or constraint named 'isa', you could use the following:
 
  # Matches column, relationship, or constraints named 'isa':
  '*:*/isa'
 
-Different delimiter characters are used for the source level (C<':'>) and the type level (C<'/'>) so you
-can do things like match any column/relationship/constraint of a specific source, such as:
+Different delimiter characters are used for the source level (C<':'>) and the type level (C<'/'>) 
+so you can do things like match any column/relationship/constraint of a specific source, such as:
 
  Artist:code
 
@@ -499,14 +505,14 @@ items are changed.
 
 =item *
 
-Also, there is no deeper "diff" for 'added' and 'deleted' events because it is redundant. For an added source,
-for example, you already know that every column, relationship, etc., that it contains is also "added" 
-(depending on your definition of "added"), so these are not included in the diff for the purpose of 
-reducing clutter. But, one side effect of this that you have to keep in mind is that when filtering 
-for all 'column' changes, for example, this will not include columns in added sources. This is just 
-a design decision made early on (and it can't be both ways). It just means if you want to check for
-the expanded definition of 'modified' columns, which include added/deleted via a source, you must also test
-for added/deleted sources.
+Also, there is no deeper "diff" for 'added' and 'deleted' events because it is redundant. For an 
+added source, for example, you already know that every column, relationship, etc., that it contains 
+is also "added" (depending on your definition of "added"), so these are not included in the diff 
+for the purpose of reducing clutter. But, one side effect of this that you have to keep in mind is 
+that when filtering for all changes to 'columns', for example, this will not include columns in 
+added sources. This is just a design decision made early on (and it can't be both ways). It just 
+means if you want to check for the expanded definition of 'modified' columns, which include 
+added/deleted via a source, you must also test for added/deleted sources.
 
 In a later version, an additional layer of sugar methods could be added to provide convenient access
 to some of these concepts.
@@ -538,9 +544,10 @@ trivial. This is more about how DBIC works than anything else.
 
 One thing that I did to overcome this when there were lots of different loaded components that I
 couldn't do anything about was to deploy both sides to a temp SQLite file, then create new schemas
-from those files with L<Schema::Loader|DBIx::Class::Schema::Loader>, using the same options (and 
-thus the same loaded components), and then run the diff on the two I<new> schemas. This type of approach
-may not work in all scenarios; it obviously depends on what exactly you are trying to accomplish.
+(in memory) from those files with L<Schema::Loader|DBIx::Class::Schema::Loader>, using the same 
+options (and thus the same loaded components), and then run the diff on the two I<new> schemas. 
+This type of approach may not work or be appropriate in all scenarios; it obviously depends on 
+what exactly you are trying to accomplish.
 
 
 =back
