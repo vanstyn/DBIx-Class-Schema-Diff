ee66864edc976c899bc3faca53db08807e2914ac
Henry Van Styn <vanstyn@intellitree.com>
2014-04-22 11:16:02 -0400

    More progress on FILTERING section of POD

--- lib/DBIx/Class/Schema/Diff.pm
+++ lib/DBIx/Class/Schema/Diff.pm
@@ -277,21 +277,20 @@ See L<FILTERING|DBIx::Class::Schema::Diff#FILTERING> for filter argument syntax.
 =head1 FILTERING
 
 The L<filter|DBIx::Class::Schema::Diff#filter> (and inverse L<filter_out|DBIx::Class::Schema::Diff#filter_out>) 
-method(s) is analogous to the L<search|DBIx::Class::ResultSet#search>
-method of L<ResultSet|DBIx::Class::ResultSet> in that it is chainable (returns a new object instance) and each 
-call further restricts the data returned. Except, instead of building up an SQL query, it filters out the 
-data in the L<diff> HashRef. The filter argument(s) define an expression which matches specific parts of 
-the C<diff> packet. In the case of C<filter()>, all data that B<does not> match the expression is removed, 
-while in the case of C<filter_out()>, all data that B<does> match the expression is removed.
+method is analogous to ResultSet's L<search_rs|DBIx::Class::ResultSet#search_rs> in that it is chainable 
+(i.e. returns a new object instance) and each call further restricts the data considered. But, instead of 
+building up an SQL query, it filters the data in the HashRef returned by C<diff()>. The filter 
+argument(s) define an expression which matches specific parts of the C<diff> packet. In the case of 
+C<filter()>, all data that B<does not> match the expression is removed from the HashRef (of the returned,
+new object), while in the case of C<filter_out()>, all data that B<does> match the expression is removed.
 
 The filter expression is designed to be simple and declarative. It can be supplied as a list of strings
-which match either broadly or narrowly the schema data to consider. The filter strings follow this
-general pattern:
+which match schema data either broadly or narrowly. A filter string argument follows this general pattern:
 
  '<source>:<type>/<id>'
 
-Where C<source> is the name of a specific schema source, C<type> is the type of data, which is currently 
-one of five (5) supported types, I<'columns'>, I<'relationships'>, I<'constraints'>, I<'isa'> or I<'table_name'>,
+Where C<source> is the name of a specific source in the schema (either side), C<type> is the I<type> of data, which is currently 
+one of five (5) supported, predefined types: I<'columns'>, I<'relationships'>, I<'constraints'>, I<'isa'> and I<'table_name'>,
 and C<id> is the name of an item, specific to that type, if applicable. 
 
 For instance, this expression would match only the I<column> named 'timestamp' in the source named 'Artist':
@@ -299,8 +298,8 @@ For instance, this expression would match only the I<column> named 'timestamp' i
  'Artist:column/timestamp'
 
 Not all types have sub-items (only I<columns>, I<relationships> and I<constraints>). The I<isa> and I<table_name>
-types are only source-specific. So, for example, to see changes to I<isa> (i.e. differences in inheritance and/or
-loaded components in the result class) you would use the following:
+types are source-global. So, for example, to see changes to I<isa> (i.e. differences in inheritance and/or
+loaded components in the result class) you could use the following:
 
  'Artist:isa'
 
@@ -316,17 +315,67 @@ C<extra> (which is where DBIC puts the list of possible values for enum columns)
 
 The structure is specific to the type. The dot-separated path applies to the data returned by L<column_info|DBIx::Class::ResultSource#column_info> for columns and
 L<relationship_info|DBIx::Class::ResultSource#relationship_info> for relationships. For instance, 
-the following matches changes to C<cascade_delete> of a specific relationship 'some_rel' in the 'Artist'
+the following matches changes to C<cascade_delete> of a specific relationship named 'some_rel' in the 'Artist'
 source:
 
  'Artist:relationship/some_rel.attrs.cascade_delete'
 
-...
+Filter arguments can also match I<broadly> using the wildcard asterisk character (C<*>). For instance, to match
+I<'isa'> changes in any source:
+
+ '*:isa'
+
+The system also accepts ambiguous/partial match strings and tries to "DWIM". So, the above can also 
+be written simply as:
+
+ 'isa'
+
+This is possible because 'isa' is understood/known as a I<type> keyword. Additionally, the system knows the names
+of all the sources in advance, so the following filter string argument would match everything in the 'Artist'
+source:
+
+ 'Artist'
+
+Sub-item names are automatically resolved, too. The following would match any column, relationship, or
+constraint named C<'code'> in any source:
+
+ 'code'
+
+When you have schemas with overlapping names, such as a column named 'isa', you simply need to supply more
+specific match strings, as ambiguous names are resolved with left-precedence. So, to match any
+column, relationship, or constraint named 'isa', you could use the following:
+
+ # Matches column, relationship, or constraints named 'isa':
+ '*:*/isa'
+
+Different delimiter characters are used for the source level (C<':'>) and the type level (C<'/'>) so you
+can do things like match any column/relationship/constraint of a specific source, such as:
+
+ Artist:code
+
+The above is equivalent to:
+
+ Artist:*/code
+
+You can also supply a delimiter character to match a specific level explicitly. So, if you wanted to
+match all changes to a I<source> named 'isa':
+
+ # Matches a source (poorly) named 'isa'
+ 'isa:'
+
+The same works at the type level. The following are all equivalent
+
+ # Each of the following 3 filter strings are equivalent:
+ 'columns/'
+ '*:columns/*'
+ 'columns'
 
 Internally, L<Hash::Layout> is used to process the filter arguments.
 
-
 =head2 event filtering
+
+Besides matching specific parts of the schema, you can also filter by I<event>, which are I<'added'>, 
+I<'deleted'> or I<'changed'>.
 
 ...
 
